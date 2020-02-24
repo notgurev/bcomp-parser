2db64cf47972d7a3692df634865d71d3f270693b
notgurev <notgurev@gmail.com>
2020-02-24 15:16:34 +0300

    def empty functions

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 input_file = open('input.txt', 'r')
 output_file = open('output.txt', 'w')
+
+def adr_com():
+    print()
+
+def bez_adr_com():
+    print()
+
+def vet_com():
+    print()
+
+with input_file as s:
+    for line in s:
